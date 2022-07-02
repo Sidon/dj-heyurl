f436e821b96c453b7da2e558916fe2e23fdf13e5
Sidon <sidoncd@gmail.com>
2022-07-02 18:10:00 -0300

    add coveragerc

--- tests/test_views.py
+++ tests/test_views.py
@@ -3,8 +3,8 @@ from heyurl import views
 from heyurl.utils import helper
 from .mocks import Mocks
 
-
 mocks = Mocks()
+
 
 @pytest.fixture
 def patch_user_agent(monkeypatch):
@@ -16,17 +16,20 @@ def test_store(django_db_setup):
     response = views.store(mocks.request_new)
     assert response.status_code == 200
 
+
 @pytest.mark.django_db
 def test_short_url(patch_user_agent, django_db_setup):
     # Redirect
     response = views.redirect_short_url(mocks.request, helper.short_url)
     assert response.status_code == 302 and response.url == helper.original_url
 
+
 @pytest.mark.django_db
 def test_handler404(django_db_setup, patch_user_agent, monkeypatch):
     monkeypatch.setattr('heyurl.views.render', lambda request, template: mocks.request())
     response = views.handler404(mocks.request(), None)
     assert response.url == helper.original_url
+
 
 @pytest.mark.django_db
 def test_month_metrics(django_db_setup):
