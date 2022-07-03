1a0f567e8d50516fdb72d37246cc498112890ee2
Sidon <sidoncd@gmail.com>
2022-07-03 11:48:11 -0300

    refactoring KISS

--- tests/mocks.py
+++ tests/mocks.py
@@ -3,6 +3,7 @@ from heyurl.utils import helper
 
 
 browser = namedtuple('Browser', ['family'], defaults=['Chrome'])
+
 user_agent = namedtuple(
     'UserAgent',
     ['browser', 'is_pc', 'is_mobile', 'is_tablet'],
@@ -10,7 +11,9 @@ user_agent = namedtuple(
 )
 
 post = namedtuple('Post', ['get'], defaults=[lambda original_url=helper.original_url: None])
+
 new_post = post(lambda original_url: helper.new_url)
+
 get = namedtuple('Get', ['get'], defaults=[lambda *args: helper.short_url if args[0] == 'short_url' else None])
 
 request = namedtuple(
